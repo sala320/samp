fd0e4e895759902e6fdcdaab729e6be278bf3ba8
Wadud Jandali <wjandali@gmail.com>
2012-09-20 10:38:10 -0700

    finished layout and routes -wtf? 5.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 SampleAppl::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  
+  match '/signup', to: 'users_#new'
 
-  get "static_pages/about"
+  match '/about', to: 'static_pages#about'
+  
+  match '/contact', to: 'static_pages#contact'
 
-  get "static_pages/contact"
+  match '/help', to: 'static_pages#help'
+  
+  root to: 'static_pages#home'
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
