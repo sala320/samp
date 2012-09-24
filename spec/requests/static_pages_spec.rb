b94cc59cc4a51913b6cdfd3c5ffd6319a11b27f1
Wadud Jandali <wjandali@gmail.com>
2012-09-24 02:10:36 -0700

    ch 6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,31 +3,37 @@ require 'spec_helper'
 
 
 describe "Static pages" do
+  subject {page}
   
+  shared_examples_for "all static pages" do
+    it {should have_selector('h1', text:h1)}
+    it {should have_selector('title', text: title)}
+  end
+    
   #let(:base_title){'Ruby on Rails Tutorial Sample App | '}
   
    describe "Contact page" do
      
     before(:each){visit contact_path}
-
-    it "should have the h1 'Contact'" do
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      page.should have_selector('title',
-                    text: full_title('Contact'))
-    end
+    let(:h1){'Contact'}
+    let(:title) {full_title('Contact')}
+    
+    it_should_behave_like "all static pages"
   end
 
   describe "Home page" do
     before(:each) {visit root_path}
-    subject {page}
-
-    it {should have_selector('h1', text: 'Sample App')}
+    let(:h1){'Sample App'}
+    let(:title){'Home'}
+    it_should_behave_like "all static pages"
     
-    it {should have_selector('title', text: full_title('Home'))}
-
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About Us')
+    end
+    
+    it {should_not have_selector 'title', text: '| Homex'}  
   end
 
   describe "Help page" do
